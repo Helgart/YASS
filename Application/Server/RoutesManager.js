b906b074f843b171b075a3249c6c6f8bd3073cc2
Michael FORASTE <michael.foraste@gmail.com>
2014-09-26 18:00:35 +0200

    fix async routes loading, now sync

--- Application/Server/RoutesManager.js
+++ Application/Server/RoutesManager.js
@@ -7,19 +7,22 @@ module.exports = {
     filename: '',
 
     load: function (path, filename, app) {
+
         var self = this;
+
         this.path = process.cwd() + '/' + path;
         this.filename = filename;
-        fs.readdir(path, function (err, files) {
-            if (files === undefined) {
-                return;
+        var files = fs.readdirSync(path);
+
+        if (files === undefined) {
+            return;
+        }
+
+        for (var i = 0, len = files.length; i < len; i++) {
+            if (fs.existsSync(self.path + '/' + files[i] + '/' + filename + '.js')) {
+                require(self.path + '/' + files[i] + '/' + filename)(app);
             }
-            for (var i = 0, len = files.length; i < len; i++) {
-                if (fs.existsSync(self.path + '/' + files[i] + '/' + filename + '.js')) {
-                    require(self.path + '/' + files[i] + '/' + filename)(app);
-                }
-            }
-        });
+        }
     }
 
 };
